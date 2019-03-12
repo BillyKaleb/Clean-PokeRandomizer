493518f4124bad0c55842b267a3a136323f3886f
billyhananto <billykaleb@yahoo.com>
2019-03-12 14:32:20 +0700

    Create application-level class

--- app/src/main/java/com/kaleb/clean_pokerandomizer/Main/MainActivity.java
+++ app/src/main/java/com/kaleb/clean_pokerandomizer/Main/MainActivity.java
@@ -1,4 +1,6 @@
-package com.kaleb.clean_pokerandomizer;
+package com.kaleb.clean_pokerandomizer.Main;
+
+import com.kaleb.clean_pokerandomizer.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ app/src/main/java/com/kaleb/clean_pokerandomizer/PokeRandomizerApplication.java
@@ -0,0 +1,17 @@
+package com.kaleb.clean_pokerandomizer;
+
+import android.app.Application;
+
+/**
+ * @author Billy Kaleb Hananto (billy.hananto@dana.id)
+ * @version PokeRandomizerApplication, v 0.1 12/03/19 14.27 by Billy Kaleb Hananto
+ */
+public class PokeRandomizerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
